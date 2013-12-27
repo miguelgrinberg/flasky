f9bfdb272ca24bed292a15dfc38e3e3ba29fd33e
Miguel Grinberg <miguelgrinberg50@gmail.com>
2013-12-27 02:31:00 -0800

    Chapter 7: Large file structure (7a)

---
+++ app/main/views.py
@@ -0,0 +1,27 @@
+from flask import render_template, session, redirect, url_for, current_app
+from .. import db
+from ..models import User
+from ..email import send_email
+from . import main
+from .forms import NameForm
+
+
+@main.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            session['known'] = False
+            if current_app.config['FLASKY_ADMIN']:
+                send_email(current_app.config['FLASKY_ADMIN'], 'New User',
+                           'mail/new_user', user=user)
+        else:
+            session['known'] = True
+        session['name'] = form.name.data
+        return redirect(url_for('.index'))
+    return render_template('index.html',
+                           form=form, name=session.get('name'),
+                           known=session.get('known', False))
