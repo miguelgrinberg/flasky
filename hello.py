b39bcb57c366f4f8fbaf48c769e18ca7fa2f850c
Miguel Grinberg <miguelgrinberg50@gmail.com>
2013-12-26 23:04:50 -0800

    Chapter 2: Dynamic routes (2b)

--- hello.py
+++ hello.py
@@ -7,5 +7,10 @@ def index():
     return '<h1>Hello World!</h1>'
 
 
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, %s!</h1>' % name
+
+
 if __name__ == '__main__':
     app.run(debug=True)
