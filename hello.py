c2a213b4fb393dbb5d549eed0adc9f95d70fe199
Miguel Grinberg <miguelgrinberg50@gmail.com>
2013-12-26 23:31:24 -0800

    Chapter 3: Custom error pages (3c)

--- hello.py
+++ hello.py
@@ -8,6 +8,16 @@ manager = Manager(app)
 bootstrap = Bootstrap(app)
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
