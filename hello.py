9f8898f09ebce35c409defaa8a511d2fcc36ef17
Miguel Grinberg <miguelgrinberg50@gmail.com>
2013-12-26 23:01:48 -0800

    Chapter 3: Templates (3a)

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask.ext.script import Manager
 
 app = Flask(__name__)
@@ -8,12 +8,12 @@ manager = Manager(app)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('index.html')
 
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, %s!</h1>' % name
+    return render_template('user.html', name=name)
 
 
 if __name__ == '__main__':
