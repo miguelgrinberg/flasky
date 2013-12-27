a6f303c81c00b55cbcd08e802c94708c36c0d1f7
Miguel Grinberg <miguelgrinberg50@gmail.com>
2013-12-26 23:12:43 -0800

    Chapter 3: Templates with Flask-Bootstrap (3b)

--- hello.py
+++ hello.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
 from flask_script import Manager
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 
 manager = Manager(app)
+bootstrap = Bootstrap(app)
 
 
 @app.route('/')
