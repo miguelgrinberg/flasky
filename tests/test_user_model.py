2bf7718df37f076e80a01ea67772fe917d09bdc3
Miguel Grinberg <miguelgrinberg50@gmail.com>
2013-12-29 00:10:10 -0800

    Chapter 8: Account confirmation (8e)

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -1,4 +1,5 @@
 import unittest
+import time
 from app import create_app, db
 from app.models import User
 
@@ -33,3 +34,27 @@ class UserModelTestCase(unittest.TestCase):
         u = User(password='cat')
         u2 = User(password='cat')
         self.assertTrue(u.password_hash != u2.password_hash)
+
+    def test_valid_confirmation_token(self):
+        u = User(password='cat')
+        db.session.add(u)
+        db.session.commit()
+        token = u.generate_confirmation_token()
+        self.assertTrue(u.confirm(token))
+
+    def test_invalid_confirmation_token(self):
+        u1 = User(password='cat')
+        u2 = User(password='dog')
+        db.session.add(u1)
+        db.session.add(u2)
+        db.session.commit()
+        token = u1.generate_confirmation_token()
+        self.assertFalse(u2.confirm(token))
+
+    def test_expired_confirmation_token(self):
+        u = User(password='cat')
+        db.session.add(u)
+        db.session.commit()
+        token = u.generate_confirmation_token(1)
+        time.sleep(2)
+        self.assertFalse(u.confirm(token))
