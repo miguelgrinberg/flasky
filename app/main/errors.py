1ee657728ace01abbcf1560bb7de97444def2ca8
Miguel Grinberg <miguelgrinberg50@gmail.com>
2013-12-29 23:21:22 -0800

    Chapter 9: User roles and permissions (9a)

--- app/main/errors.py
+++ app/main/errors.py
@@ -2,6 +2,11 @@ from flask import render_template
 from . import main
 
 
+@main.app_errorhandler(403)
+def forbidden(e):
+    return render_template('403.html'), 403
+
+
 @main.app_errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
