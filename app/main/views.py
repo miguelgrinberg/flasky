7d4341f04dabd05e8cc70e3d636d1b27c75a0e75
Miguel Grinberg <miguelgrinberg50@gmail.com>
2013-12-30 00:11:00 -0800

    Chapter 10: User profiles (10a)

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,14 @@
-from flask import render_template
+from flask import render_template, abort
 from . import main
+from ..models import User
 
 
 @main.route('/')
 def index():
     return render_template('index.html')
+
+
+@main.route('/user/<username>')
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    return render_template('user.html', user=user)
