6d83f22f8685f94a2f4a3a00da22a14aafb571eb
Miguel Grinberg <miguelgrinberg50@gmail.com>
2013-12-30 00:11:00 -0800

    Chapter 10: User profiles (10a)

--- app/auth/views.py
+++ app/auth/views.py
@@ -11,11 +11,12 @@ from .forms import LoginForm, RegistrationForm, ChangePasswordForm,\
 
 @auth.before_app_request
 def before_request():
-    if current_user.is_authenticated \
-            and not current_user.confirmed \
-            and request.endpoint[:5] != 'auth.' \
-            and request.endpoint != 'static':
-        return redirect(url_for('auth.unconfirmed'))
+    if current_user.is_authenticated:
+        current_user.ping()
+        if not current_user.confirmed \
+                and request.endpoint[:5] != 'auth.' \
+                and request.endpoint != 'static':
+            return redirect(url_for('auth.unconfirmed'))
 
 
 @auth.route('/unconfirmed')
