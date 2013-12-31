90367217eec1e48795df7ada4f475d0decf40427
Miguel Grinberg <miguelgrinberg50@gmail.com>
2013-12-31 01:57:14 -0800

    Chapter 11: Blog post pagination (11d)

--- config.py
+++ config.py
@@ -14,6 +14,7 @@ class Config:
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
     FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+    FLASKY_POSTS_PER_PAGE = 20
 
     @staticmethod
     def init_app(app):
