d9b89f7efeb40813f7d393accd807f953e0196f8
Miguel Grinberg <miguelgrinberg50@gmail.com>
2013-12-31 13:12:30 -0800

    Chapter 11: Blog post editor (11h)

--- app/main/views.py
+++ app/main/views.py
@@ -86,3 +86,20 @@ def edit_profile_admin(id):
 def post(id):
     post = Post.query.get_or_404(id)
     return render_template('post.html', posts=[post])
+
+
+@main.route('/edit/<int:id>', methods=['GET', 'POST'])
+@login_required
+def edit(id):
+    post = Post.query.get_or_404(id)
+    if current_user != post.author and \
+            not current_user.can(Permission.ADMINISTER):
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.body = form.body.data
+        db.session.add(post)
+        flash('The post has been updated.')
+        return redirect(url_for('.post', id=post.id))
+    form.body.data = post.body
+    return render_template('edit_post.html', form=form)
