ee9549ab0a6d57d3795b73f0423550e3cd480c2a
Miguel Grinberg <miguelgrinberg50@gmail.com>
2013-12-31 03:27:41 -0800

    Chapter 11: Rich text blog posts with Flask-PageDown (11e)

--- app/__init__.py
+++ app/__init__.py
@@ -4,12 +4,14 @@ from flask_mail import Mail
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_pagedown import PageDown
 from config import config
 
 bootstrap = Bootstrap()
 mail = Mail()
 moment = Moment()
 db = SQLAlchemy()
+pagedown = PageDown()
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
@@ -26,6 +28,7 @@ def create_app(config_name):
     moment.init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
+    pagedown.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
