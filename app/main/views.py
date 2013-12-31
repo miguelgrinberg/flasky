124001959cf85b4227628898825087467c7858ab
Miguel Grinberg <miguelgrinberg50@gmail.com>
2013-12-31 03:53:54 -0800

    Chapter 11: Permanent links to posts (11g)

--- app/main/views.py
+++ app/main/views.py
@@ -80,3 +80,9 @@ def edit_profile_admin(id):
     form.location.data = user.location
     form.about_me.data = user.about_me
     return render_template('edit_profile.html', form=form, user=user)
+
+
+@main.route('/post/<int:id>')
+def post(id):
+    post = Post.query.get_or_404(id)
+    return render_template('post.html', posts=[post])
