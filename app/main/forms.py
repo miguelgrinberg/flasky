f8aea807147e8908373c0cd8c75a03c263a96836
Miguel Grinberg <miguelgrinberg50@gmail.com>
2014-01-01 12:10:12 -0800

    Chapter 13: Blog post comments (13a)

--- app/main/forms.py
+++ app/main/forms.py
@@ -53,3 +53,8 @@ class EditProfileAdminForm(FlaskForm):
 class PostForm(FlaskForm):
     body = PageDownField("What's on your mind?", validators=[Required()])
     submit = SubmitField('Submit')
+
+
+class CommentForm(FlaskForm):
+    body = StringField('Enter your comment', validators=[Required()])
+    submit = SubmitField('Submit')
