347b2c8f93290961781d4e0ec41f5178e0f0caea
Miguel Grinberg <miguelgrinberg50@gmail.com>
2013-12-31 16:17:29 -0800

    Chapter 12: Database representaton of followers (12a)

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import os
 from app import create_app, db
-from app.models import User, Role, Permission, Post
+from app.models import User, Follow, Role, Permission, Post
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
 
@@ -11,8 +11,8 @@ migrate = Migrate(app, db)
 
 
 def make_shell_context():
-    return dict(app=app, db=db, User=User, Role=Role, Permission=Permission,
-                Post=Post)
+    return dict(app=app, db=db, User=User, Follow=Follow, Role=Role,
+                Permission=Permission, Post=Post)
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
