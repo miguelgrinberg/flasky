30d223a94bbdcf31e6b857f47ad58613d16e95fb
Miguel Grinberg <miguelgrinberg50@gmail.com>
2014-01-01 12:10:12 -0800

    Chapter 13: Blog post comments (13a)

--- config.py
+++ config.py
@@ -15,6 +15,7 @@ class Config:
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     FLASKY_POSTS_PER_PAGE = 20
     FLASKY_FOLLOWERS_PER_PAGE = 50
+    FLASKY_COMMENTS_PER_PAGE = 30
 
     @staticmethod
     def init_app(app):
