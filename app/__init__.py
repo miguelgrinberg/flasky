ba0a7bdd774233916f968006c3aee7e2d467d94a
Miguel Grinberg <miguelgrinberg50@gmail.com>
2014-01-02 23:08:16 -0800

    Chapter 14: API (14a)

--- app/__init__.py
+++ app/__init__.py
@@ -36,4 +36,7 @@ def create_app(config_name):
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
+    from .api_1_0 import api as api_1_0_blueprint
+    app.register_blueprint(api_1_0_blueprint, url_prefix='/api/v1.0')
+
     return app
