627bbf629f88f95e8b19ae1e84aca90d9ece0885
Miguel Grinberg <miguelgrinberg50@gmail.com>
2014-01-02 23:08:16 -0800

    Chapter 14: API (14a)

---
+++ app/api_1_0/decorators.py
@@ -0,0 +1,14 @@
+from functools import wraps
+from flask import g
+from .errors import forbidden
+
+
+def permission_required(permission):
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            if not g.current_user.can(permission):
+                return forbidden('Insufficient permissions')
+            return f(*args, **kwargs)
+        return decorated_function
+    return decorator
