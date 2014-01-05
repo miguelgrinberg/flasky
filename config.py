9bcab0e17504629b4bfe7ca557f1fc44d920f767
Miguel Grinberg <miguelgrinberg50@gmail.com>
2014-01-05 03:20:35 -0800

    Chapter 17: Traditional hosting (17d)

--- config.py
+++ config.py
@@ -85,11 +85,25 @@ class HerokuConfig(ProductionConfig):
         app.logger.addHandler(file_handler)
 
 
+class UnixConfig(ProductionConfig):
+    @classmethod
+    def init_app(cls, app):
+        ProductionConfig.init_app(app)
+
+        # log to syslog
+        import logging
+        from logging.handlers import SysLogHandler
+        syslog_handler = SysLogHandler()
+        syslog_handler.setLevel(logging.WARNING)
+        app.logger.addHandler(syslog_handler)
+
+
 config = {
     'development': DevelopmentConfig,
     'testing': TestingConfig,
     'production': ProductionConfig,
     'heroku': HerokuConfig,
+    'unix': UnixConfig,
 
     'default': DevelopmentConfig
 }
