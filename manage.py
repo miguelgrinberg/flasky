469bd04aeea01a1d514b2532be7d305aaca691a2
Miguel Grinberg <miguelgrinberg50@gmail.com>
2014-01-05 02:11:42 -0800

    Chapter 16: Source code profiling (16b)

--- manage.py
+++ manage.py
@@ -45,5 +45,14 @@ def test(coverage=False):
         COV.erase()
 
 
+@manager.command
+def profile(length=25, profile_dir=None):
+    """Start the application under the code profiler."""
+    from werkzeug.contrib.profiler import ProfilerMiddleware
+    app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[length],
+                                      profile_dir=profile_dir)
+    app.run()
+
+
 if __name__ == '__main__':
     manager.run()
