e4f1c1eaa4bcc7dd133704d6ef49da8586b45b82
Miguel Grinberg <miguelgrinberg50@gmail.com>
2014-01-04 23:45:18 -0800

    Chapter 15: Unit tests with Selenium (15d)

--- app/main/views.py
+++ app/main/views.py
@@ -9,6 +9,17 @@ from ..models import Permission, Role, User, Post, Comment
 from ..decorators import admin_required, permission_required
 
 
+@main.route('/shutdown')
+def server_shutdown():
+    if not current_app.testing:
+        abort(404)
+    shutdown = request.environ.get('werkzeug.server.shutdown')
+    if not shutdown:
+        abort(500)
+    shutdown()
+    return 'Shutting down...'
+
+
 @main.route('/', methods=['GET', 'POST'])
 def index():
     form = PostForm()
