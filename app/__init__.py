09aad43ea683b8eead6edb4b9c6f4df6fe8bb147
Miguel Grinberg <miguelgrinberg50@gmail.com>
2014-01-05 02:38:01 -0800

    Chapter 17: Heroku support (17c)

--- app/__init__.py
+++ app/__init__.py
@@ -30,6 +30,10 @@ def create_app(config_name):
     login_manager.init_app(app)
     pagedown.init_app(app)
 
+    if not app.debug and not app.testing and not app.config['SSL_DISABLE']:
+        from flask_sslify import SSLify
+        sslify = SSLify(app)
+
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
