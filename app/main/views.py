bebc1714e8c29d9f59b74850a4430593413db7ac
Ivan Dyedov <idyedov@50onred.com>
2014-07-12 19:31:51 -0400

    don't include a route to shutdown the entire server, even if it's only for unit tests better to just kill the app thread when tests finish

--- app/main/views.py
+++ app/main/views.py
@@ -21,17 +21,6 @@ def after_request(response):
     return response
 
 
-@main.route('/shutdown')
-def server_shutdown():
-    if not current_app.testing:
-        abort(404)
-    shutdown = request.environ.get('werkzeug.server.shutdown')
-    if not shutdown:
-        abort(500)
-    shutdown()
-    return 'Shutting down...'
-
-
 @main.route('/', methods=['GET', 'POST'])
 def index():
     form = PostForm()
