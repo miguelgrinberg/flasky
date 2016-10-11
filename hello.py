df7093a21f0daaebd48e927cb027410fc6fcff82
Kyle Zheng <eusers@qq.com>
2016-10-11 11:14:37 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 
@@ -14,7 +14,7 @@ bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 
-class NameForm(Form):
+class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[Required()])
     submit = SubmitField('Submit')
 
