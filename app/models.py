07d50e6d7ffc8eb4c8321c5a9a6dffad0e8bf799
Miguel Grinberg <miguel.grinberg@gmail.com>
2017-07-18 07:55:23 -0700

    Chapter 8: Password resets (8g)

--- app/models.py
+++ app/models.py
@@ -51,6 +51,24 @@ class User(UserMixin, db.Model):
         db.session.add(self)
         return True
 
+    def generate_reset_token(self, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'reset': self.id}).decode('utf-8')
+
+    @staticmethod
+    def reset_password(token, new_password):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token.encode('utf-8'))
+        except:
+            return False
+        user = User.query.get(data.get('reset'))
+        if user is None:
+            return False
+        user.password = new_password
+        db.session.add(user)
+        return True
+
     def __repr__(self):
         return '<User %r>' % self.username
 
