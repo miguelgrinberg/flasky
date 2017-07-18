abf6d4d4c12a639b2bae0bb136e2a794eba0c35c
Miguel Grinberg <miguel.grinberg@gmail.com>
2017-07-18 07:55:50 -0700

    Chapter 16: Logging of slow database queries (16a)

--- config.py
+++ config.py
@@ -14,9 +14,11 @@ class Config:
     FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_RECORD_QUERIES = True
     FLASKY_POSTS_PER_PAGE = 20
     FLASKY_FOLLOWERS_PER_PAGE = 50
     FLASKY_COMMENTS_PER_PAGE = 30
+    FLASKY_SLOW_DB_QUERY_TIME = 0.5
 
     @staticmethod
     def init_app(app):
