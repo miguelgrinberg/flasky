a3deb3ff209209e32a1ad2377fbf9a69e3029a14
Miguel Grinberg <miguel.grinberg@gmail.com>
2017-07-18 07:55:31 -0700

    Chapter 11: Blog posts in profile pages (11b)

--- app/main/views.py
+++ app/main/views.py
@@ -23,7 +23,8 @@ def index():
 @main.route('/user/<username>')
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    return render_template('user.html', user=user)
+    posts = user.posts.order_by(Post.timestamp.desc()).all()
+    return render_template('user.html', user=user, posts=posts)
 
 
 @main.route('/edit-profile', methods=['GET', 'POST'])
