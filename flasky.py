036bae9803daf06d74df982a49d063ac79e88066
Miguel Grinberg <miguel.grinberg@gmail.com>
2017-07-18 07:55:30 -0700

    Chapter 11: Blog posts (11a)

--- flasky.py
+++ flasky.py
@@ -1,7 +1,7 @@
 import os
 from flask_migrate import Migrate
 from app import create_app, db
-from app.models import User, Role, Permission
+from app.models import User, Role, Permission, Post
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
@@ -9,7 +9,7 @@ migrate = Migrate(app, db)
 
 @app.shell_context_processor
 def make_shell_context():
-    return dict(db=db, User=User, Role=Role, Permission=Permission)
+    return dict(db=db, User=User, Role=Role, Permission=Permission, Post=Post)
 
 
 @app.cli.command()
