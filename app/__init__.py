6bacad4cbd8454a94b58700936e1ee3fcbcb14ad
Miguel Grinberg <miguel.grinberg@gmail.com>
2017-07-18 07:55:55 -0700

    Chapter 17: Heroku support with Waitress (17c-waitress)

--- app/__init__.py
+++ app/__init__.py
@@ -29,6 +29,10 @@ def create_app(config_name):
     login_manager.init_app(app)
     pagedown.init_app(app)
 
+    if app.config['SSL_REDIRECT']:
+        from flask_sslify import SSLify
+        sslify = SSLify(app)
+
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
