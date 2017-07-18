f0d6546112480b0f728194982e3fa8f9d7f0962f
Miguel Grinberg <miguel.grinberg@gmail.com>
2017-07-18 07:55:57 -0700

    Chapter 17: Traditional hosting (17g)

--- flasky.py
+++ flasky.py
@@ -1,4 +1,9 @@
 import os
+from dotenv import load_dotenv
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
 
 COV = None
 if os.environ.get('FLASK_COVERAGE'):
