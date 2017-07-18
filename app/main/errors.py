0f60402c6b11e7348762b4cdfeb0b329f09f0877
Miguel Grinberg <miguel.grinberg@gmail.com>
2017-07-18 07:55:15 -0700

    Chapter 7: Large file structure (7a)

---
+++ app/main/errors.py
@@ -0,0 +1,12 @@
+from flask import render_template
+from . import main
+
+
+@main.app_errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@main.app_errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
---
+++ app/main/forms.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
