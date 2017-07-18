97e2ef9ff38d5bea15ca5b92e3acde4b59a19c5d
Miguel Grinberg <miguel.grinberg@gmail.com>
2017-07-18 07:55:25 -0700

    Chapter 9: User roles and permissions (9a)

--- flasky.py
+++ flasky.py
@@ -2,7 +2,7 @@ import os
 import click
 from flask_migrate import Migrate
 from app import create_app, db
-from app.models import User, Role
+from app.models import User, Role, Permission
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
@@ -10,7 +10,7 @@ migrate = Migrate(app, db)
 
 @app.shell_context_processor
 def make_shell_context():
-    return dict(db=db, User=User, Role=Role)
+    return dict(db=db, User=User, Role=Role, Permission=Permission)
 
 
 @app.cli.command()
