809d39f9b375276d4236d25c7aeaf3a7a1739652
Miguel Grinberg <miguel.grinberg@gmail.com>
2017-07-18 07:55:24 -0700

    Chapter 8: Email address changes (8h)

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -55,3 +55,14 @@ class PasswordResetForm(FlaskForm):
         DataRequired(), EqualTo('password2', message='Passwords must match')])
     password2 = PasswordField('Confirm password', validators=[DataRequired()])
     submit = SubmitField('Reset Password')
+
+
+class ChangeEmailForm(FlaskForm):
+    email = StringField('New Email', validators=[DataRequired(), Length(1, 64),
+                                                 Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Update Email Address')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data.lower()).first():
+            raise ValidationError('Email already registered.')
