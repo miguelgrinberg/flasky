37836543d4a9569a3d750c440256170a63000e30
Miguel Grinberg <miguel.grinberg@gmail.com>
2017-07-18 07:55:38 -0700

    Chapter 12: Database representaton of followers (12a)

--- flasky.py
+++ flasky.py
@@ -2,7 +2,7 @@ import os
 import click
 from flask_migrate import Migrate
 from app import create_app, db
-from app.models import User, Role, Permission, Post
+from app.models import User, Follow, Role, Permission, Post
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
@@ -10,7 +10,8 @@ migrate = Migrate(app, db)
 
 @app.shell_context_processor
 def make_shell_context():
-    return dict(db=db, User=User, Role=Role, Permission=Permission, Post=Post)
+    return dict(db=db, User=User, Follow=Follow, Role=Role,
+                Permission=Permission, Post=Post)
 
 
 @app.cli.command()
