31406715b1ea058da7ad4a0e435c55c6f7433691
Miguel Grinberg <miguel.grinberg@gmail.com>
2017-07-18 07:55:45 -0700

    Chapter 15: Coverage metrics (15a)

--- flasky.py
+++ flasky.py
@@ -1,4 +1,13 @@
 import os
+
+COV = None
+if os.environ.get('FLASK_COVERAGE'):
+    import coverage
+    COV = coverage.coverage(branch=True, include='app/*')
+    COV.start()
+
+import sys
+import click
 from flask_migrate import Migrate
 from app import create_app, db
 from app.models import User, Follow, Role, Permission, Post, Comment
@@ -14,8 +23,25 @@ def make_shell_context():
 
 
 @app.cli.command()
-def test():
+@click.option('--coverage/--no-coverage', default=False,
+              help='Run tests under code coverage.')
+def test(coverage):
     """Run the unit tests."""
+    if coverage and not os.environ.get('FLASK_COVERAGE'):
+        import subprocess
+        os.environ['FLASK_COVERAGE'] = '1'
+        sys.exit(subprocess.call(sys.argv))
+
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+    if COV:
+        COV.stop()
+        COV.save()
+        print('Coverage Summary:')
+        COV.report()
+        basedir = os.path.abspath(os.path.dirname(__file__))
+        covdir = os.path.join(basedir, 'tmp/coverage')
+        COV.html_report(directory=covdir)
+        print('HTML version: file://%s/index.html' % covdir)
+        COV.erase()
