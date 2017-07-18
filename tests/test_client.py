ec4af47fdde58ff9bfabcd5832e181ae232c2964
Miguel Grinberg <miguel.grinberg@gmail.com>
2017-07-18 07:55:48 -0700

    Chapter 15: API testing with the Flask test client (15c)

--- tests/test_client.py
+++ tests/test_client.py
@@ -20,7 +20,7 @@ class FlaskClientTestCase(unittest.TestCase):
     def test_home_page(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertTrue(b'Stranger' in response.data)
+        self.assertTrue('Stranger' in response.get_data(as_text=True))
 
     def test_register_and_login(self):
         # register a new account
@@ -38,9 +38,11 @@ class FlaskClientTestCase(unittest.TestCase):
             'password': 'cat'
         }, follow_redirects=True)
         self.assertEqual(response.status_code, 200)
-        self.assertTrue(re.search(b'Hello,\s+john!', response.data))
+        self.assertTrue(re.search('Hello,\s+john!',
+                                  response.get_data(as_text=True)))
         self.assertTrue(
-            b'You have not confirmed your account yet' in response.data)
+            'You have not confirmed your account yet' in response.get_data(
+                as_text=True))
 
         # send a confirmation token
         user = User.query.filter_by(email='john@example.com').first()
@@ -50,9 +52,11 @@ class FlaskClientTestCase(unittest.TestCase):
         user.confirm(token)
         self.assertEqual(response.status_code, 200)
         self.assertTrue(
-            b'You have confirmed your account' in response.data)
+            'You have confirmed your account' in response.get_data(
+                as_text=True))
 
         # log out
         response = self.client.get('/auth/logout', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
-        self.assertTrue(b'You have been logged out' in response.data)
+        self.assertTrue('You have been logged out' in response.get_data(
+            as_text=True))
