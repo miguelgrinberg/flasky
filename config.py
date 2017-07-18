29e3646db8185254f0c9f52522014f83ed095ece
Miguel Grinberg <miguel.grinberg@gmail.com>
2017-07-18 07:55:57 -0700

    Chapter 17: Traditional hosting (17g)

--- config.py
+++ config.py
@@ -102,12 +102,26 @@ class DockerConfig(ProductionConfig):
         app.logger.addHandler(file_handler)
 
 
+class UnixConfig(ProductionConfig):
+    @classmethod
+    def init_app(cls, app):
+        ProductionConfig.init_app(app)
+
+        # log to syslog
+        import logging
+        from logging.handlers import SysLogHandler
+        syslog_handler = SysLogHandler()
+        syslog_handler.setLevel(logging.INFO)
+        app.logger.addHandler(syslog_handler)
+
+
 config = {
     'development': DevelopmentConfig,
     'testing': TestingConfig,
     'production': ProductionConfig,
     'heroku': HerokuConfig,
     'docker': DockerConfig,
+    'unix': UnixConfig,
 
     'default': DevelopmentConfig
 }
