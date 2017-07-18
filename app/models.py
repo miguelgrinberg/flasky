299baf1f6f67d1f98863bbebe3caded2c136f726
Miguel Grinberg <miguel.grinberg@gmail.com>
2017-07-18 07:55:42 -0700

    Chapter 12: Self-followers (12e)

--- app/models.py
+++ app/models.py
@@ -105,6 +105,14 @@ class User(UserMixin, db.Model):
                                 lazy='dynamic',
                                 cascade='all, delete-orphan')
 
+    @staticmethod
+    def add_self_follows():
+        for user in User.query.all():
+            if not user.is_following(user):
+                user.follow(user)
+                db.session.add(user)
+                db.session.commit()
+
     def __init__(self, **kwargs):
         super(User, self).__init__(**kwargs)
         if self.role is None:
@@ -114,6 +122,7 @@ class User(UserMixin, db.Model):
                 self.role = Role.query.filter_by(default=True).first()
         if self.email is not None and self.avatar_hash is None:
             self.avatar_hash = self.gravatar_hash()
+        self.follow(self)
 
     @property
     def password(self):
