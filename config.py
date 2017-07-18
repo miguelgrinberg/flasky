af584e018e14f67ba1a99f3d1b3b165a30f83760
Miguel Grinberg <miguel.grinberg@gmail.com>
2017-07-18 07:55:43 -0700

    Chapter 13: Blog post comments (13a)

--- config.py
+++ config.py
@@ -16,6 +16,7 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     FLASKY_POSTS_PER_PAGE = 20
     FLASKY_FOLLOWERS_PER_PAGE = 50
+    FLASKY_COMMENTS_PER_PAGE = 30
 
     @staticmethod
     def init_app(app):
