85ed2ece7f8ea267ac712937a45c95679cbcb482
Miguel Grinberg <miguel.grinberg@gmail.com>
2017-07-18 07:55:43 -0700

    Chapter 13: Blog post comments (13a)

--- flasky.py
+++ flasky.py
@@ -2,7 +2,7 @@ import os
 import click
 from flask_migrate import Migrate
 from app import create_app, db
-from app.models import User, Follow, Role, Permission, Post
+from app.models import User, Follow, Role, Permission, Post, Comment
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
@@ -11,7 +11,7 @@ migrate = Migrate(app, db)
 @app.shell_context_processor
 def make_shell_context():
     return dict(db=db, User=User, Follow=Follow, Role=Role,
-                Permission=Permission, Post=Post)
+                Permission=Permission, Post=Post, Comment=Comment)
 
 
 @app.cli.command()
