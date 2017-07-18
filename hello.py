32acca2eb5b3eff63536f4022ffa96c7570d1b68
Miguel Grinberg <miguel.grinberg@gmail.com>
2017-07-17 23:21:48 -0700

    Chapter 2: Dynamic routes (2b)

--- hello.py
+++ hello.py
@@ -5,3 +5,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return '<h1>Hello World!</h1>'
+
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, {}!</h1>'.format(name)
