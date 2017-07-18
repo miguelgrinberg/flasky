98b306e55c45104bbac726711eaef8fae088bbe6
Miguel Grinberg <miguel.grinberg@gmail.com>
2017-07-18 07:55:04 -0700

    Chapter 3: Dates and times with Flask-Moment (3e)

--- hello.py
+++ hello.py
@@ -1,9 +1,12 @@
+from datetime import datetime
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
 
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 
 @app.errorhandler(404)
@@ -18,7 +21,8 @@ def internal_server_error(e):
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',
+                           current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
