2929b031fcd9f30cf1dbadab9d420d29dc4d3372
congyingTech <congyingTech@163.com>
2018-03-13 11:53:04 +0800

    Update forms.py
    Regexp has three parameter---regex, flags=0, message=None, message should be a type of string.

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -19,8 +19,7 @@ class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[
         DataRequired(), Length(1, 64),
         Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
-               'Usernames must have only letters, numbers, dots or '
-               'underscores')])
+               'Usernames must have only letters, numbers, dots or underscores')])
     password = PasswordField('Password', validators=[
         DataRequired(), EqualTo('password2', message='Passwords must match.')])
     password2 = PasswordField('Confirm password', validators=[DataRequired()])
