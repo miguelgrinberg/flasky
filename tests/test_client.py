b8b2fedddc7207bcec15496b41dde06e4b2a625c
rtharayil <ranjiththarayil@gmail.com>
2021-06-14 16:31:38 +0530

    Update test_client.py

--- tests/test_client.py
+++ tests/test_client.py
@@ -21,6 +21,11 @@ class FlaskClientTestCase(unittest.TestCase):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertTrue('Stranger' in response.get_data(as_text=True))
+        
+     def test_home_page_fail(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 202)
+        self.assertTrue('Stranger' in response.get_data(as_text=True))
 
     def test_register_and_login(self):
         # register a new account
