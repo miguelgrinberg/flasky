51f97f768ac3c26c99d2fe7e62e50de0fc242d9f
Marek Bladowski <bitemnet@gmail.com>
2024-06-21 21:36:47 +0200

    Inicjalizacja

--- hello.py
+++ hello.py
@@ -1,14 +1,3 @@
 from flask import Flask
 
 app = Flask(__name__)
-print(__name__)
-
-
-@app.route('/')
-def index():
-    return "<h1>Witaj, Świecie!</h1>"
-
-
-@app.route('/user/<name>')
-def user(name):
-    return f"<h1>Witaj, {name}!</h1>"
